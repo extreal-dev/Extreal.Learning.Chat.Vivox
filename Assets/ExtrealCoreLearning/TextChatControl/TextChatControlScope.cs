431217790f6ee8c8dd03010744e4d8d0225740dd
kiyohome <kiyohito.itoh@gmail.com>
2022-12-21 13:39:20 +0900

    学習開始時点のプロジェクトを追加

---
+++ Assets/ExtrealCoreLearning/TextChatControl/TextChatControlScope.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace ExtrealCoreLearning.TextChatControl
+{
+    public class TextChatControlScope : LifetimeScope
+    {
+        [SerializeField] private TextChatControlView textChatControlView;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterComponent(textChatControlView);
+
+            builder.RegisterEntryPoint<TextChatControlPresenter>();
+        }
+    }
+}
---
+++ Assets/ExtrealCoreLearning/VoiceChatControl/VoiceChatChannel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UniRx;
+
+namespace ExtrealCoreLearning.VoiceChatControl
+{
+    public class VoiceChatChannel : IDisposable
+    {
+        public IObservable<bool> IsMute => isMute;
+        private ReactiveProperty<bool> isMute = new ReactiveProperty<bool>();
+
+        public VoiceChatChannel()
+        {
+            isMute.Value = true;
+        }
+
+        public void ToggleMute()
+        {
+            isMute.Value = !isMute.Value;
+        }
+
+        public void Dispose()
+        {
+            isMute.Dispose();
+        }
+    }
+}
